26d2a9250fa571666efdc02a1742dce2a81ee753
L0RD-ZER0 <68327382+L0RD-ZER0@users.noreply.github.com>
2023-06-11 13:03:19 +0530

    Fix reference in ``basic-use`` example.

--- example/basic-use.ts
+++ example/basic-use.ts
@@ -1,4 +1,4 @@
 import deref from 'data-deref';
 
-deref('./example/basic-use/file.json#localref').then(console.log);
+deref('./example/basic-use/file.json').then(console.log);
 // { sendTo: 'Sender', message: 'Hello, World!', localref: 'Hello, World!' }
